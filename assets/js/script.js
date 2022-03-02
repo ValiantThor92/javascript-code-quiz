01c93c3a97861ceec418e2d778c6588250f110ba
unknown <travislnelson92@gmail.com>
2022-03-02 14:49:59 -0800

    add basic style and js arrays for q&a, add html

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var startBtn = document.getElementById("start-quiz");
+var form = document.getElementById("form");
+var userInput = document.getElementById("user-input");
+
+
+var questionArray = [
+    [],
+    [],
+    [],
+    [],
+];
+
+var answerArray = [
+    [],
+    [],
+    [],
+    [],
+];

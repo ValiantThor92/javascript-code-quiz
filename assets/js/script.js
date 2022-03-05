b883d6e670de15d216ad230b991edd8efcc041f6
unknown <travislnelson92@gmail.com>
2022-03-04 19:40:49 -0800

    add endgame() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 var startBtn = document.getElementById("start-quiz");
 var quiz = document.getElementById("quiz");
 var quizForm = document.getElementById("quiz-form");
+
 var question = document.getElementById("question");
 var userAnswer = document.getElementById("user-Answer");
 var questions = document.getElementById("questions");
+
 var timer = document.getElementById("timer");
+
 var highScoreForm = document.getElementById("high-score-form");
 var highScoreBtn = document.getElementById("high-score")
+
 var allRadios = document.getElementById("answer");
 var allLabels = document.getElementById("radios").querySelectorAll("label");
+
+var endScreenHeader = document.createElement("h2");
+var endScreen = document.createElement("p");
 
 var restartBtn = document.createElement("button");
 // set bootstrapping for the restart btn
@@ -24,10 +31,10 @@ var questionArray = [
 ];
 
 var answerArray = [
-    ["<scripting>", "<javascript>", "<script>", "<js>"],
-    ["The <head> section", "both the <head> section and the <body> section are correct", "the <body> section", "A <p> element"],
-    ["<script name=`xxx.js`", "<script src=`xxx.js`>", "<script href=`xxx.js`>", "<script rel=`xxx.js`"],
-    ["true", "false"],
+    ["<scripting>", "<javascript>", "<script>", "<js>", "</script>"],
+    ["The <head> section", "both the <head> section and the <body> section are correct", "the <body> section", "A <p> element", "the <body> section" ],
+    ["<script name=`xxx.js`", "<script src=`xxx.js`>", "<script href=`xxx.js`>", "<script rel=`xxx.js`", "<script src=`xxx.js`>"],
+    ["true", "false", "false"],
 ];
 
 var countdown;
@@ -73,6 +80,33 @@ function startQuiz() {
 
     else {
         endGame(true);
+    }
+
+};
+
+function endGame(win) {
+
+    game = false;
+    quizForm.setAttribute("style", "display: none");
+    clearInterval(countdown);
+    document.getElementById("timer-name").setAttribute("style", "display: none");
+
+    if(win) {
+        endScreenHeader.textContent = "Winner!";
+        endScreen.textContent = "Your score is " + timer;
+        document.getElementById("win").append(endScreenHeader);
+        document.getElementById("win").append(endScreen);
+        playerScore = timer;
+        highScoreForm.setAttribute("style", "display: inline");
+    }
+
+    else {
+        endScreenHeader.textContent = "Times up";
+        endScreen.textContent = "try again to get the high score!";
+        document.getElementById("win").append(endScreenHeader);
+        document.getElementById("win").append(endScreen);
+        playerScore = timer;
+        highScoreForm.setAttribute("style", "display: inline");
     }
 
 };

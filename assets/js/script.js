870218e61d626aeed0be6f1d4139dabd99340eae
unknown <travislnelson92@gmail.com>
2022-03-13 17:40:57 -0700

    debug logic, alter style implementation, add style to HS.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,24 @@
-var startBtn = document.getElementById("start-quiz");
-var cardPointer = document.getElementById("quiz");
-var quizForm = document.getElementById("quiz-form");
+var questionsContainerEl = document.querySelector("#questions");
 
-var question = document.getElementById("question");
-var userAnswer = document.getElementById("user-Answer");
-var questions = document.getElementById("questions");
+var promptEl = document.querySelector("#question-prompt");
+var choicesEl = document.querySelector("#question-choices");
 
-var timerDisplay = document.getElementById("timer");
+var finalScoreEl = document.querySelector("#final-score");
+var timerEl = document.querySelector("#countdown");
 
-var highScoreForm = document.getElementById("high-score-form");
-var highScoreBtn = document.getElementById("high-score")
+var feedbackEl = document.getElementById("right-wrong");
+var highScoreScreenEl = document.getElementById("highscore-input");
+var initialsEl = document.getElementById("player");
+
+var startBtn = document.querySelector("#start");
+var submitBtn = document.querySelector("#submit")
+
+var currentQuestion = 0;
+var time = 100;
+var timer;
 
 
-
-
-var endScreenHeader = document.createElement("h2");
-var endScreen = document.createElement("p");
-
-var restartBtn = document.createElement("button");
-// set bootstrapping for the restart btn
-restartBtn.setAttribute("class", "btn btn-outline-success");
-// set type attribute for the restart btn
-restartBtn.setAttribute("type", "button");
-
-var questionArray = [
+var questions = [
 
     {
         prompt: "Inside which HTML element do we put the JavaScript?",
@@ -51,153 +46,122 @@ var questionArray = [
 
 ];
 
-
-
-var countdown;
-var timer = 120;
-var q = 0;
-var game = false;
-var playerScore;
-
 var startQuiz = function() {
-
-
-
-};
-
-function endGame(win) {
-
-    game = false;
-    quizForm.setAttribute("style", "display: none");
-    clearInterval(countdown);
-    document.getElementById("timer-name").setAttribute("style", "display: none");
-
-    if(win) {
-        endScreenHeader.textContent = "Winner!";
-        endScreen.textContent = "Your score is " + timer;
-        document.getElementById("win").append(endScreenHeader);
-        document.getElementById("win").append(endScreen);
-        playerScore = timer;
-        highScoreForm.setAttribute("style", "display: inline");
-    }
-
-    else {
-        endScreenHeader.textContent = "Times up";
-        endScreen.textContent = "try again to get the high score!";
-        document.getElementById("win").append(endScreenHeader);
-        document.getElementById("win").append(endScreen);
-        playerScore = timer;
-        highScoreForm.setAttribute("style", "display: inline");
-    }
+    
+    var startInfoEl = document.querySelector(".start-info");
+    startInfoEl.setAttribute("class", "hide");
+    questionsContainerEl.removeAttribute("class");
+    timer = setInterval(clock, 1000);
+    timerEl.textContent = time;
+    displayQuestion();
 
 };
 
-function checkAnswer() {
+var displayQuestion = function() {
 
-     //If the user's answer is correct, increment q and call the startQuiz main function, which will load the next question
-    if(quizForm.answer.value === "correct") {
-        q++;
-        startQuiz();
-    }
+    var questionIndex = questions[currentQuestion];
+    promptEl.textContent = questionIndex.prompt;
+    choicesEl.innerHTML = "";
 
-    else {
-        timer -= 10;
-        document.getElementById("user-answer").setAttribute("class", "btn btn-danger")
+    questionIndex.choices.forEach(function(choices, i) {
 
-        var flashRed = setTimeout(function() {
-            document.getElementById("user-answer").setAttribute("class", "btn btn-primary")
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice btn btn-outline-primary")
+        choiceBtn.setAttribute("value", choices);
+        choiceBtn.textContent = i + 1 + ". " + choices;
+        choiceBtn.onclick = choiceClick;
+        choicesEl.appendChild(choiceBtn);
 
-        }, 250)
-
-    }
+    });
 
 }
 
-function startTimer() {
+var choiceClick = function() {
 
-    countdown = setInterval(() => {
-        timer--;
-        timerDisplay.textContent = timer;
+    if (this.value !== questions[currentQuestion].answer) {
 
-        if(timer <= 0) {
-            clearInterval(countdown);
-            timer = 0;
-            timerDisplay.textContent = timer;
-            endGame(false);
+        time -= 10;
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Wrong!";
 
-        }
+    } else {
+
+        feedbackEl.textContent = "Correct!";
+
+    };
+
+    feedbackEl.setAttribute("class", "feedback");
+
+    setTimeout(function() {
+
+        feedbackEl.setAttribute("class", "feedback hide");
 
     }, 1000);
 
-}
+    currentQuestion++;
 
-function postScore() {
-    // stores user input [key] and score [value] in local storage
-    localStorage.setItem(highScoreForm.pname.value, playerScore);
-    // reload page to try again or check highscores
-    location.reload();
-}
+    setTimeout(() => {
 
-function loadHighScoreTable() {
-    document.getElementById("timer-name").setAttribute("style", "display: none");
+        if (currentQuestion === questions.length) {
 
-    // fetch scores from local storage
-    var allHighScores = {...localStorage};
-    var orderedHighScores = [];
+            endQuiz();
 
-    // for/in loop to iterate through the object properties and create key:value array
-    for (var scores in allHighScores) {
-        orderedHighScores.push([scores, allHighScores[scores]]);
+    } else {
 
-    }
+        displayQuestion();
 
-    // sorts scores in ascending order
-    orderedHighScores.sort(function(a, b) {
-        return a[1] - b[1];
-    });
+    };
 
-    // hide start and highScoreBtn
-    startBtn.setAttribute("style", "display: none");
-    highScoreBtn.setAttribute("style", "display: none");
+    }, 1000);
 
-    // append the high scores h2
-    endScreenHeader.textContent = "High Scores";
-    cardPointer.append(endScreenHeader);
+};
 
-    // initiate counter at 1
-    var k = 1
-    
-    // iterate backwards to list highest first
-    for(var i = orderedHighScores.length-1; i >= 0; i--) {
+var clock = function() {
 
-        // limits high score list to 5 inputs
-        if(k < 6) {
-            // create <p> element and add player name and score
-            var j = document.createElement("p");
-            j.textContent = k + orderedHighScores[i][0] + orderedHighScores[i][1];
+    time--;
+    timerEl.textContent = time;
 
-             // add bootstrapping to <p> element
-            j.setAttribute("class", "border m1 rounded border-success");
-            //  append <p> element to high score table
-            cardPointer.append(j);
+    if (time <= 0) {
 
-            k++;
-
-        }
+        endQuiz();
 
     }
 
-    restartBtn.textContent = "Go back";
-    cardPointer.append(restartBtn);
+};
 
-}
+var endQuiz = function() {
+    clearInterval(timer);
 
-// event listeners for highscore, start quiz btn, and restart button(or go back)
+    questionsContainerEl.setAttribute("class", "hide");
 
-highScoreBtn.addEventListener("click", loadHighScoreTable);
-startBtn.addEventListener("click", startQuiz);
-restartBtn.addEventListener("click", function() {
-    location.reload();
+    highScoreScreenEl.removeAttribute("class");
 
-});
+    finalScoreEl.textContent = time;
+};
 
+var saveScore = function() {
+
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var userScore = {
+
+            score: time,
+            initials: initials
+
+        };
+
+        highscores.push(userScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+        window.location.href = "highscores.html";
+        window.location.reload();
+
+    };
+
+};
+
+submitBtn.onclick = saveScore;
+startBtn.onclick = startQuiz;
